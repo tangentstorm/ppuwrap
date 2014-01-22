72ef3763706044d2c221e1f133ee3a38b34b80f4
tangentstorm <michal.wallace@gmail.com>
2014-01-21 21:12:59 -0600

    new app: ppuxml2lib. shows help, reads xml.

---
+++ ppuxml2lib.pas
@@ -0,0 +1,38 @@
+{ ppuxml2lib : generated shared libraries from free pascal units
+  --------------------------------------------------------------
+  Copyright (c) 2014 michal j wallace <http://tangentstorm.com/>
+  Available under the MIT license. See LICENSE file for details.
+  -------------------------------------------------------------- }
+{$mode objfpc}{$m+}{$h+}
+program ppuxml2lib;
+uses xpath, xmlread, DOM;
+
+procedure help;
+  begin
+    writeln('usage:');
+    writeln('ppuxml2lib [-py] [-h] dumpfile.xml');
+    writeln('  -py  : generate python module');
+    writeln('  -h   : show this help file');
+    writeln('generate dumpfile with the ppudump program');
+    writeln('that ships with free pascal 2.7.1 or newer');
+    writeln('  ppudump -FX myunit.xml');
+    halt;
+  end;
+
+procedure main(dumpfile : string; genpy: boolean);
+  var doc : DOM.TXMLDocument;
+  begin
+    xmlread.ReadXMLFile( doc, dumpfile );
+  end;
+
+var
+  i : integer; genpy : boolean = false; dumpfile : string='';
+begin
+  if paramcount > 0 then for i := 1 to paramcount do
+    case paramstr(i) of
+      '-h'  : help;
+      '-py' : genpy := true;
+      else dumpfile := paramstr(i)
+    end; { case }
+  if dumpfile = '' then help else main(dumpfile, genpy)
+end.
